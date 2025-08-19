47935b2f30a022825689861200466ad2700b014f
rf777rf777 <rf777rf777@gmail.com>
2025-08-20 00:55:20 +0800

    Update stylish button function & remove classic button

--- src/ui.js
+++ src/ui.js
@@ -1,6 +1,6 @@
 export function getUsernames() {
   const inputs = document.querySelectorAll('.username');
   return Array.from(inputs)
-    .map(el => el.value.trim())
+    .map(el => el.innerHTML.trim())
     .filter(name => name.length > 0);
 }
